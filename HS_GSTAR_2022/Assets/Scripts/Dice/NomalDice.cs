2eda93516c322dd41eb659d7b0db366972ac6269
G1Se0k <jangisuk2@naver.com>
2022-09-30 13:43:25 +0900

    주사위 애니메이션 수정

--- HS_GSTAR_2022/Assets/Scripts/Dice/NomalDice.cs
+++ HS_GSTAR_2022/Assets/Scripts/Dice/NomalDice.cs
@@ -7,5 +7,21 @@ public class NomalDice : Dice
     public override void Roll()
     {
         Number = (EDiceNumber) Random.Range((int) EDiceNumber.One, (int) EDiceNumber.Max);
+        Play_Ani(Number);
+        Debug.Log(Number);
+    }
+
+    private void Play_Ani(EDiceNumber num)
+    {
+        GetComponent<Animator>().Play("Cube|Result_" + ((int)num).ToString(),-1,0f);
+
+    }
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Roll();
+        }
     }
 }
