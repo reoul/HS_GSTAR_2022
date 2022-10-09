15610a6f7ddc6b2034546d76113b23d1264823b5
Reoul <dydqja1358@gmail.com>
2022-10-10 07:57:12 +0900

    Logger Assert 추가
    Logger Assert 추가

--- HS_GSTAR_2022/Assets/Scripts/Logger.cs
+++ HS_GSTAR_2022/Assets/Scripts/Logger.cs
@@ -13,17 +13,17 @@ public sealed class Logger
     {
         Debug.Log(message, context);
     }
-    
+
     public static void LogFormat(string message, params object[] args)
     {
         Debug.LogFormat(message, args);
     }
-    
+
     public static void LogFormat(Object context, string message, params object[] args)
     {
         Debug.LogFormat(context, message, args);
     }
-    
+
     public static void LogWarning(object message)
     {
         Debug.LogWarning(message);
@@ -33,17 +33,17 @@ public sealed class Logger
     {
         Debug.LogWarning(message, context);
     }
-    
+
     public static void LogWarningFormat(string message, params object[] args)
     {
         Debug.LogWarningFormat(message, args);
     }
-    
+
     public static void LogWarningFormat(Object context, string message, params object[] args)
     {
         Debug.LogWarningFormat(context, message, args);
     }
-    
+
     public static void LogError(object message)
     {
         Debug.LogError(message);
@@ -53,12 +53,12 @@ public sealed class Logger
     {
         Debug.LogError(message, context);
     }
-    
+
     public static void LogErrorFormat(string message, params object[] args)
     {
         Debug.LogErrorFormat(message, args);
     }
-    
+
     public static void LogErrorFormat(Object context, string message, params object[] args)
     {
         Debug.LogErrorFormat(context, message, args);
@@ -68,10 +68,30 @@ public sealed class Logger
     {
         Debug.LogException(exception);
     }
-    
+
     public static void LogException(System.Exception exception, Object context)
     {
         Debug.LogException(exception, context);
+    }
+
+    public static void Assert(bool condition)
+    {
+        Debug.Assert(condition);
+    }
+
+    public static void Assert(bool condition, Object context)
+    {
+        Debug.Assert(condition, context);
+    }
+
+    public static void AssertFormat(bool condition, string message, params object[] args)
+    {
+        Debug.AssertFormat(condition, message, args);
+    }
+
+    public static void AssertFormat(bool condition, Object context, string message, params object[] args)
+    {
+        Debug.AssertFormat(condition, context, message, args);
     }
 #else
     public static void Log(object message) {}
@@ -88,5 +108,9 @@ public sealed class Logger
     public static void LogErrorFormat(Object context, string message, params object[] args) {}
     public static void LogException(System.Exception exception) {}
     public static void LogException(System.Exception exception, Object context) {}
+    public static void Assert(bool condition) {}
+    public static void Assert(bool condition, Object context) {}
+    public static void AssertFormat(bool condition, string message, params object[] args) {}
+    public static void AssertFormat(bool condition, Object context, string message, params object[] args) {}
 #endif
 }
