8676a1ccee294b7c67bb14b58463c09c8e844b5f
G1Se0k <jangisuk2@naver.com>
2022-10-31 15:12:35 +0900

    값 업데이터 추가
    commit eac67ef041fc21123f67fb1c267079b9fed38abd
    Author: G1Se0k <jangisuk2@naver.com>
    Date:   Mon Oct 31 15:12:19 2022 +0900
    
        값 업데이터 추가

---
+++ HS_GSTAR_2022/Assets/Scripts/ValueUpdater.cs
@@ -0,0 +1,86 @@
+using UnityEngine;
+using TMPro;
+
+public class ValueUpdater : MonoBehaviour
+{
+    [SerializeField]
+    private TMP_Text powText, piercingText, defText;
+
+    struct ValStruct {
+        public TMP_Text text;
+        public float val;
+        public float targetVal;
+
+        public ValStruct(TMP_Text tmpText)
+        {
+            text = tmpText;
+            val = System.Convert.ToInt32(tmpText.text);
+            targetVal = val;
+        }
+    }
+
+    ValStruct powVal, piercingVal, defVal;
+
+    public enum valType{ pow, piercing, def }
+
+    private void Start()
+    {
+        powVal = new ValStruct(powText);
+        piercingVal = new ValStruct(piercingText);
+        defVal = new ValStruct(defText);
+    }
+
+    private void Update()
+    {
+        valUpdate(ref powVal);
+        valUpdate(ref piercingVal);
+        valUpdate(ref defVal);
+
+        if (Input.GetKeyDown(KeyCode.D))
+        {
+            AddVal(10, valType.pow);
+        }
+        if (Input.GetKeyDown(KeyCode.F))
+        {
+            AddVal(-10, valType.pow);
+        }
+    }
+
+    private void valUpdate(ref ValStruct valStruct)
+    {
+        valStruct.val = Mathf.Lerp(valStruct.val, valStruct.targetVal, 0.025f);
+
+        valStruct.text.text = Mathf.Round(valStruct.val).ToString();
+
+        
+        if (System.Convert.ToInt32(valStruct.text.text) > valStruct.targetVal)
+        {
+            valStruct.text.color = Color.red;
+        }
+        else if(System.Convert.ToInt32(valStruct.text.text) < valStruct.targetVal)
+        {
+            valStruct.text.color = Color.green;
+        }
+        else
+        {
+            valStruct.text.color = Color.white;
+        }
+    }
+
+
+    public void AddVal(int val, valType type)
+    {
+        switch (type)
+        {
+            case valType.pow:
+                powVal.targetVal += val;
+                break;
+            case valType.piercing:
+                piercingVal.targetVal += val;
+                break;
+            case valType.def:
+                defVal.targetVal += val;
+                break;
+        }
+    }
+}
