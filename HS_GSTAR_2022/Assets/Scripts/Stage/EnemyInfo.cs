056b35a178bf03a278e8bf682277367e7ad56cfa
nutticocoa <101963067+nutticocoa@users.noreply.github.com>
2022-11-08 16:45:05 +0900

    enemy 인포 수정2

--- HS_GSTAR_2022/Assets/Scripts/Stage/EnemyInfo.cs
+++ HS_GSTAR_2022/Assets/Scripts/Stage/EnemyInfo.cs
@@ -1,6 +1,12 @@
 using System;
 using UnityEditor;
 using UnityEngine;
+public enum EnemyRatingType
+{
+    [InspectorName("이지")] Easy,
+    [InspectorName("노말")] Normal,
+    [InspectorName("하드")] Hard
+}
 
 #if UNITY_EDITOR
 [CustomEditor(typeof(EnemyInfo))]
@@ -17,6 +23,7 @@ public class EnemyInfoInspector : Editor
         EditorGUILayout.PropertyField(serializedObject.FindProperty("OffensivePower"), new GUIContent("공격력"));
         EditorGUILayout.PropertyField(serializedObject.FindProperty("DefensivePower"), new GUIContent("방어력"));
         EditorGUILayout.PropertyField(serializedObject.FindProperty("PiercingDamage"), new GUIContent("관통 데미지"));
+        EditorGUILayout.PropertyField(serializedObject.FindProperty("ratingType"), new GUIContent("등급"));
 
         serializedObject.ApplyModifiedProperties();
     }
@@ -34,4 +41,5 @@ public class EnemyInfo : ScriptableObject
     public int OffensivePower;
     public int DefensivePower;
     public int PiercingDamage;
+    public EnemyRatingType ratingType;
 }
