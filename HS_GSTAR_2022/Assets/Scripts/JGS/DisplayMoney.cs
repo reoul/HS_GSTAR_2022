6014a0f983990c6484048ac8a7c5aca4eeb006b6
G1Se0k <jangisuk2@naver.com>
2022-11-11 10:58:52 +0900

    디스플레이머니 코루틴

--- HS_GSTAR_2022/Assets/Scripts/JGS/DisplayMoney.cs
+++ HS_GSTAR_2022/Assets/Scripts/JGS/DisplayMoney.cs
@@ -1,24 +1,29 @@
 using UnityEngine;
+using System.Collections;
 using TMPro;
 
 public class DisplayMoney : MonoBehaviour
 {
-    private float _displayMoney, _targetMoney;
-    private TMP_Text _text;
-
-    private void Start()
-    {
-        _text = GetComponent<TMP_Text>();
-    }
-
-    private void Update()
-    {
-        _displayMoney = Mathf.Lerp(_displayMoney, _targetMoney, Time.deltaTime);
-        _text.text = Mathf.Round(_displayMoney).ToString();
-    }
+    private int _displayMoney;
+    [SerializeField] private TMP_Text _text;
 
     public void SetTargetMoney(int money)
     {
-        _targetMoney = money;
+        StopAllCoroutines();
+        StartCoroutine(SetNewTarget(money));
+    }
+
+    private IEnumerator SetNewTarget(int targetMoney)
+    {
+        int oldMoney = _displayMoney;
+        float time = 0;
+        float calculTime = 1;
+        while(_displayMoney != targetMoney)
+        {
+            time += Time.deltaTime;
+            _displayMoney = (int)Mathf.Lerp(oldMoney, targetMoney, time / calculTime);
+            _text.text = _displayMoney.ToString();
+            yield return null;
+        }
     }
 }
