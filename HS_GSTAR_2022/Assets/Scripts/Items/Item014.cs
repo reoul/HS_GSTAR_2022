15c37ffbe935dfd083a769520b403aafd956068d
nutticocoa <101963067+nutticocoa@users.noreply.github.com>
2022-11-15 10:44:17 +0900

    회복 제곱증가 및 거인의심장 밸런스 조정

--- HS_GSTAR_2022/Assets/Scripts/Items/Item014.cs
+++ HS_GSTAR_2022/Assets/Scripts/Items/Item014.cs
@@ -7,8 +7,8 @@ public class Item014 : Item
         IBattleable PlayerBattleable = BattleManager.Instance.PlayerBattleable;
         IBattleable EnemyBattleable = BattleManager.Instance.EnemyBattleable;
 
-        PlayerBattleable.MaxHp += Mathf.FloorToInt(EnemyBattleable.MaxHp * 0.02f);
-        PlayerBattleable.Hp += Mathf.FloorToInt(EnemyBattleable.MaxHp * 0.02f);
+        PlayerBattleable.MaxHp += Mathf.FloorToInt(EnemyBattleable.MaxHp * 0.05f);
+        PlayerBattleable.Hp += Mathf.FloorToInt(EnemyBattleable.MaxHp * 0.05f);
 
         PlayerBattleable.InfoWindow.UpdateHpBar(PlayerBattleable.Hp, PlayerBattleable.MaxHp);
     }
